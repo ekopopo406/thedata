1843ddb4e3d9ed9688f31ce370ec6dd2e2122f2e
ekopopo406 <enigma_406@163.com>
2025-05-06 23:02:52 +0800

    first commit

---
+++ src/main/java/com/lab/App.java
@@ -0,0 +1,15 @@
+package com.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication( scanBasePackages = "com.lab", exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
